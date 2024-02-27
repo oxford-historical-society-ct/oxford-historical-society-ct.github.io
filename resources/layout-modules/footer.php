325d97eaf67bbcdc9d672ed04c32c6336dffc422
Jason Lydon <jlydon99@gmail.com>
2024-02-27 15:00:11 -0500

    moving site over

---
+++ resources/layout-modules/footer.php
@@ -0,0 +1,20 @@
+<footer>
+  <?php include("menu.php") ?>
+</footer>
+<ul data-menu="contact-information">
+  <li>
+    Address:
+    <address>
+      <a href="https://goo.gl/maps/ew9QTCoE32WbL4pJ6" title="View 60 Towner Lane on Google Maps" target="_blank">60 Towner Lane<br>
+        Oxford, CT, 06478</a>
+    </address>
+  </li>
+  <li>
+    Mailing address:
+    <address>
+      <span>P.O. Box 582</span><br>
+      <span>Oxford, CT, 06478</span>
+    </address>
+  </li>
+</ul>
+<small class="copyright">&copy 2023 Oxford Historical Society</small>
---
+++ resources/layout-modules/menu.php
@@ -0,0 +1,12 @@
+<ul>
+  <li><a href="/index.html" title="Go to Oxford Past Home Page">Home</a></li>
+  <li><a href="/cemeteries.html" title="Go to Oxford Past Cemeteries">Cemeteries</a></li>
+  <li><a href="/nameindex.html" title="Go to Oxford Past Genealogy Page">Genealogy</a></li>
+  <li><a href="/library.html" title="Go to Oxford Past Library Page">Library</a></li>
+  <li><a href="mailto:oxfordhistorian@aol.com" title="Email Oxford Historical Society">Email</a></li>
+  <li><a href="https://www.facebook.com/OxfordHistorical" title="Visit Oxford Historical Society on Facebook">
+    <svg class="icon-facebook" version="1.1" viewBox="0 0 35 35" height="25" width="25">
+      <path d="M 17.500224,0 A 17.5,17.5 0 0 0 0,17.499708 17.5,17.5 0 0 0 14.632183,34.762736 l -0.03876,-12.281421 -4.370276,-0.04134 v -4.610571 h 4.355809 l -0.01241,-3.942395 c 0,0 -1.205169,-6.414601 7.416601,-6.414595 2.672697,0 3.073715,0.267684 3.073715,0.267684 l 0.267166,4.342887 -2.672705,0.06666 c 0,0 -2.205032,-0.200083 -2.205032,2.071709 0,0.389443 -0.0081,2.426338 -0.01138,3.608049 h 4.55476 L 24.254833,22.5733 20.419925,22.53661 c -0.01855,5.695323 -0.03719,11.715798 -0.03876,12.224575 A 17.5,17.5 0 0 0 34.999919,17.499708 17.5,17.5 0 0 0 17.500211,0 Z" />
+    </svg>
+  </a></li>
+</ul>
