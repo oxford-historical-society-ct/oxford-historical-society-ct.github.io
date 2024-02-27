c42b599d8541dc7f2ba3bc733021044e4a4a39f2
Jason Lydon <jlydon99@gmail.com>
2024-02-27 11:52:56 -0500

    moving site over

---
+++ index-2023.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Oxford Past - Cemetery, Genealogy and History Project - Oxford, New Haven, Connecticut</title>
+        <link rel="stylesheet" href="/resources/styles/layout.css">
+    </head>
+    <body data-page-template="home">      
+        <?php include($_SERVER['DOCUMENT_ROOT']."/resources/layout-modules/header.php") ?>
+        <article>
+            <section class="full">
+                <h3 class="full">Oxford Past is under construction! Learn more at <a href="http://oxford-historical-society.org/" title="Visit the Oxford Historical Society website">the Oxford Historical Society website</a></h3>
+            </section>
+            <section class="full">
+                <div class="full">
+                    <p><strong>Join the Oxford Historical Society now!</strong></p>
+                    <p>The Society has worked hard to move, renovate, restore and preserve Mr. Munn’s 1850’s one-room schoolhouse. As we get ready to host students at the “new” facility, we’re inviting folks to join with us in this exciting program.</p>
+                    <p>Don’t miss the opportunity for preserving Oxford’s rural heritage and sharing the fun of local history with Oxford’s school students. Plus, we have lots of programs planned for all ages!</p>
+                    <p><a href="/resources/pdfs/oxford-hitorical-society-membership-form-2023.pdf" title="Download our membership form" target="_blank">Download our membership form today!</a></p>
+                </div>
+                <figure>
+                    <img src="/resources/graphics/newletters/2023.01/preparing-to-welcome-students.jpg" alt="Preparing to welcome student to Mr. Munn’s 1850’s one-room schoolhouse">
+                    <figcaption>Preparing to welcome student to Mr. Munn’s 1850’s one-room schoolhouse</figcaption>
+                </figure>
+                <figure>
+                    <img src="/resources/graphics/newletters/2023.01/oxford-historical-society-ramp.jpg" alt="Mr Munn's school house has a new ramp!">
+                    <figcaption>Mr Munn's school house has a new ramp!</figcaption>
+                </figure>
+            </section>
+            <section>
+                <p><strong>THANKS FOR YOUR SUPPORT!</strong></p>
+                <img src="/resources/graphics/newletters/2023.01/giving-tuesday.jpg" alt="Giving Tuesday Logo">
+                <p>Our campaign for Giving Tuesday 2022 met our goal of 12,000. Over the two weeks following Nov. 30, we had donations of mailed checks, and gifts via our <a href="https://www.facebook.com/OxfordHistorical" title="Visit our page on Facebook">FaceBook page</a> and Paypal. We started with a matching challenge of 1500 from a retired town educator and met that in the first 48hours! As money continued to come in we collected funds needed for the student supplies to use in our restored Munn Schoolhouse. We are hopeful the doors will open this spring!</p>
+                <p>See the article at right for an important way YOU can help us host Oxford school children.</p>
+            </section>
+            <section>
+                <p><strong>Ion Bank 2023 Community Support Campaign</strong></p>
+                <img src="/resources/graphics/newletters/2023.01/ion-bank.jpg" alt="Community Awards Program Logo">
+                <p>We need your vote. Ion Bank’s Community Sup- port 2023 campaign offers donors the opportunity to use bank foundation funds to support local non-profits. Anyone with a bank account at Ion Bank can vote (only once) and each vote adds 125 to the society’s tally. Ac counts with multiple names allow each person one vote, but multiple accounts do not qualify for more than one vote. What better way to provide support than with someone else’s money?!</p>
+            </section>
+            <section>
+                <p><strong>Annual Fiber Fest: April 2023</strong></p>
+                <p><img src="/resources/graphics/newletters/2023.01/sheep.jpg" alt="Sheep!" class="small left">The Homestead, 60 Towner Lane Saturday April 15, 10 a.m. - 4 p.m. Sunday April 16, noon – 4 p.m.</p>
+                <p>Sheep shearing will occur multiple times, check the schedule.</p>
+                <p><img src="/resources/graphics/newletters/2023.01/wool-tool.jpg" alt="Wool!" class="small right">Free Admission. All ages are welcome to walk the grounds. Locally raised live animals will be on display including sheep, rabbits and alpaca. See our room size barn loom at work. Watch and try carding and spin- ning, see a spinning wheel in use. Try your hand at weaving on a table loom, give the drop spindle a try. Explore the 1755 Twitchell Rowland Homestead and displays. Nearly a dozen fiber crafters from around the state will be visiting and shar- ing their skills plus offering products for sale. Snacks will be available. You do not want to miss this special two day event.</p>
+            </section>
+            <section id="visit">
+                <p><strong>Visit the Twitchell-Rowland Homestead</strong></p>
+                <ul>
+                    <li>Open the 1st and 3rd Sunday every month</li>
+                    <li>2-4pm</li>
+                    <li><strong>Free admission</strong></li>
+                    <li>60 Towner Lane, Oxford - <a href="https://goo.gl/maps/ew9QTCoE32WbL4pJ6" target="_blank" title="See ">Map</a></li>
+                </ul>
+                <figure>
+                    <img src="/resources/graphics/newletters/2023.01/oxford-historical-society.jpg" alt="The 1755 Twitchell-Rowland Homestead">
+                    <figcaption>The 1755 Twitchell-Rowland Homestead</figcaption>
+                </figure>
+                <p>Drop in and experience Oxford’s living history. The 1755 Twitchell-Rowland Homestead was saved from destruction and relocated to its current site at 60 Towner Lane thanks to community support of a historic level. Pictures of the historic house move and regular monthly displays of Oxford life and living are featured. Books on Oxford are on sale, photo notecards and T-shirts sup- port the ongoing maintenance of the Homestead and Society activities.</p>
+                <p>Our current display is “Gifts from the Past”, a collection of family treasures and memories loaned by mem- bers of the Oxford Historical Society Board of Direc- tors. These vary from a lovely porcelain teapot used in a Christian Street home in the early 20th century to a wonderful toy barn with all its animal residents from 1947.</p>
+            </section>
+            <section>
+                <p><strong>Society benefits from CT Humanities Grants</strong></p>
+                <img src="/resources/graphics/newletters/2023.01/ct-humanities-cultural-fund.jpg" alt="Connecticut Humanities Cultural Fund">
+                <p>2022 saw state non-profit groups offered a helping hand in acknowledgment of the impact of the Covid pandemic severely limiting fundraising opportunities in 2020 and 2021.</p>
+                <p>Thanks to the energies and efforts of Society mem- ber Dottie DeBisschop the Oxford Historical Society applied for and received funding provided by the Connecti- cut State Department of Economic and Community De- velopment/Connecticut Office of the Arts (COA) from the Connecticut State Legislature. An additional grant was provided from the CT Humanities (CTH) through the Sustaining the Humanities through the American Rescue Plan with monies from the National Endowment for the Humanities (NEH) and the federal American Res- cue Plan (ARP) Act.</p>
+                <p>These grants underwrote society operating expenses in 2022 and allowed us to launch a website upgrade project, hiring a consultant who updated and restruc- tured the society’s website and consolidated the con- tent found on the OHS web presence.</p>
+                <p>OHS has been notified that it is approved for another cycle of operating expenses support for 2023. The So- ciety is hopeful the coming year will see more opportu- nities to meet with members of the community and re- turn to events that support the society’s mission to edu- cate and preserve local history in Oxford, the lower Naugatuck Valley and the state of Connecticut.</p>
+            </section>
+            <section>
+                <p><strong>Help Wanted!</strong></p>
+                <p>Love history? Fascinated by old houses? What a great opportunity to share your passion by becoming a do- cent at The Homestead.</p>
+                <p>We will give you training and you can share the fun with other volunteers. You do not need to be a member to qualify. For more information contact: Nancy Farnum 203-888-0230.</p>
+            </section>
+            <section class="full">
+                <div>
+                    <p><strong>Heritage Recipe: Macaroni and Cheese</strong></p>
+                    <p>From the Kitchen of Laura and Bo Duncan</p>
+                    <p>William and Laura Duncan Duncan moved to the big white house with the wraparound porch at 436 Oxford Road in 1951. World War II Navy veteran William Duncan had been hired as a civil engineer for the S. B. Church Company after Mr. Church died. Laura, a gradu- ate of Amherst College, had worked in a laboratory in Boston developing latex paint before her marriage. The house was home to the couple’s three girls: Terry, Toni and Elizabeth or Bo as she was known, and their brothers Pete and Jim.</p>
+                    <p>The house was in need of many repairs, but located in a wonderful neighborhood for children. The Duncan clan played outside all day long with the Hessler, Crepeau, Schade and DeMaio families. “Our parents let us be kids which was really nice,” says Bo. The backyard was planted with a large stand of pine trees and Pete and Jim and their friends quickly learned to leap from one to another. Laura headed upstairs and pretended she didn’t know what was going on until the day when Jim grabbed a branch with a hornet’s nest. His multiple stings were treated with the family cure all of a bathtub filled with water and Epsom salts. "You can’t cry in the bathtub," Laura told them all.</p>
+                    <figure>
+                        <img src="/resources/graphics/newletters/2023.01/436-oxford-road.jpg" alt="436 Oxford Road">
+                        <figcaption>436 Oxford Road</figcaption>
+                    </figure>
+                    <p>In his free time, William Duncan undertook renovating his home. At one time anyone opening the door could look straight down into the cellar. Old walls came down, and a pile of ancient lathe provided kindling for the evening fire in the fireplace. While preparing to paper the dining room one Valentine’s Day, he drew a big heart on the wall that he emblazoned “Laura and Bill.” When Bo built her own home on Great Hill Road, she repeated the tradition on her first Christmas there, painting trees to decorate her new house. They are now covered with paint, but still there in her memory each holiday season. He also created a beautiful yard that ran back to Jack’s Brook for his family. Challenged by all the rocks when he wanted a driveway, he enlisted the children to load them into a trailer. Anyone who helped got to drive the tractor to dump them. Fall brought them all out to rake and pile leaves in the back of an S. B. Church truck in return for a ride to the compost heap.</p>
+                </div>
+                <div>
+                    <p>Bo relates that Thursday night the Duncans often had macaroni and cheese for supper. This was because Thursday was piano lesson day. “My two brothers and I each had a half hour lesson after school. While we were all doing that Mom would go grocery shopping but this left no time to make supper once we got home. Macaroni and cheese could be prepared and left in the oven to bake while we were away. Once we got home, out of the oven it came and supper was on the table.” Laura Duncan’s recipe, derived from her Fanny Farmer Cookbook, follows.</p>
+                    <figure>
+                        <img src="/resources/graphics/newletters/2023.01/mac-and-cheese.jpg" alt="The Duncan Family’s Macaroni & Cheese">
+                        <figcaption>The Duncan Family’s Macaroni & Cheese</figcaption>
+                    </figure>
+                    <p><strong>The Duncan Family’s Macaroni & Cheese</strong>
+                    <p>First make the cream sauce: in a double boiler or heavy saucepan, melt 4 tablespoons of butter. Add 4 tablespoons of flour and blend well over low heat. Add to this 2 cups of milk or milk and cream. Whisk the sauce and bring it slowly to a boil. Cook for 2 minutes stirring constantly. Season with salt and pepper.</p>
+                    <p>Now to make the Macaroni and Cheese:</p>
+                    <ul>
+                        <li>Cook a 9 oz. package of macaroni according to pack age directions.</li>
+                        <li>Put half in a buttered baking dish. Sprinkle it with 1/2 cup of cheddar cheese (or more)</li>
+                        <li>Add the rest of the macaroni. Sprinkle another 1/2 cup of cheddar cheese (or more) on top.</li>
+                        <li>Pour in the 2 cups of cream sauce.</li>
+                        <li>Put 1/2 cup of buttered bread crumbs on top.</li>
+                        <li>Bake at 400 degrees until brown, 25 – 30 minutes.</li>
+                    </ul>
+                    <p>If you are going to piano lessons then cook until you get home but at a lower temperature.</p>
+                    
+                </div>
+            </section>
+        </article>
+        <?php include($_SERVER['DOCUMENT_ROOT']."/resources/layout-modules/footer.php") ?>
+    </body>
+</html>
---
+++ index-archive.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Oxford Past - Cemetery, Genealogy and History Project - Oxford, New Haven, Connecticut</title>
+        <link rel="stylesheet" href="/resources/styles/layout.css">
+    </head>
+    <body data-page-template="home">      
+        <?php include($_SERVER['DOCUMENT_ROOT']."/resources/layout-modules/header.php") ?>
+        <article>
+            <section class="full">
+                <div class="full">
+                    <h3>As we prepare to open Mr. Munn'a one-room 1850's schoolhouse for Oxford's student programs, we seek community donations and support in purchasing supplies for the schoolhouse.</h3>
+                    <p><strong>Notes from a 1850 Christmas in 2023</strong></p>
+                    <p>Holiday decorations will once again add a seasonal touch to Oxford’s Twitchell-Rowland Homestead Museum from 2-4 p.m. on Sunday, December 4 and 18 and on January 2, 2023. The Museum is located at 60 Towner Lane, off Route 67 in Oxford.</p>
+                    <p>Of special interest is this year’s featured display “Gifts from the Past,” a collection of family treasures loaned by members of the Oxford Historical Society evoking memories of long ago. Included are a toy barn and animals from 1947,  a porcelain teapot used on Christian Street in the early 1900s, a nightgown worn by Rowland family children at the Homestead c1926, and Native American basketry and beading from the early 20th century. A lit Christmas Village completes the festive special offerings.</p>
+                    <p>Gifts such as notecards and books of local history will be for sale. Docent led tours of the museum’s three rooms will be available. Admission is free and cider and cookies will be served. The display can also be seen by special appointment by calling Nancy Farnum at 203 888-0230.</p>
+                </div>
+            </section>
+            <section>
+                <p><strong>1850's US Map</strong>
+                <figure>
+                    <img src="/resources/graphics/home/us-map-1850.jpg" alt="1850's US Map">
+                    <figcaption>1850's US Map <small>Oxford Historical Society Digital Photo Collection</small></figcaption>
+                </figure>
+                <p>Visiting students will be able to see how much the United States map has changed when they study a large 1850’s map of the United States and the various territories not yet states.</p>
+            </section>
+            <section data-media="right">
+                <p><strong>Cheaper than Paper</strong>
+                <figure>
+                    <img src="/resources/graphics/home/student-slates.jpg" alt="A student's slate from 1850">
+                    <figcaption>A student's slate from 1850 <small>Oxford Historical Society Digital Photo Collection</small></figcaption>
+                </figure>
+                <p>Students in 1850 used to small slate boards practice writing and do their math assignments, because paper was very expensive. Slate was cheaper and reusable. Actual student slates, made from stone, and edged with wood are available. The Society hopes to purchase about 25 authentic slate boards to provide an authentic 1850's school day for Oxford's children.</p>
+            </section>
+            <section>
+                <p><strong>Students in 1850 used Quill Pens and Ink Wells</strong></p>
+                <figure>
+                    <img src="/resources/graphics/home/student-quill-pens.jpg" alt="A student's Quill Pens and Ink Wells from 1850">
+                    <figcaption>A student's Quill Pens and Ink Wells from 1850 <small>Oxford Historical Society Digital Photo Collection</small></figcaption>
+                </figure>
+                <p>Students in 1850 were taught to use quill pens. Usually made from turkey feathers, the base of the feather was trimmed to make a point which could be dipped in ink for penmanship prac- tice. We hope to obtain quill pens and ink wells for our student visitors to use. Instructions for making quill pens are available or group would like to make them, or they could be purchased ready-made. We'd love to hear from someone or group who would provide them for Mr. Munn’ schoolhouse use.</p>
+            </section>
+            <section data-media="split">
+                <p><strong>Presidential Portraits</strong></p>
+                <figure>
+                    <img src="/resources/graphics/home/presidential-portrait-1.jpg" alt="Portrait of President Benjamin Harrison">
+                    <figcaption>Portrait of President Benjamin Harrison<small>Oxford Historical Society Digital Photo Collection</small></figcaption>
+                </figure>
+                <figure>
+                    <img src="/resources/graphics/home/presidential-portrait-2.jpg" alt="President George Washington">
+                    <figcaption>Portrait of President George Washington<small>Oxford Historical Society Digital Photo Collection</small></figcaption>
+                </figure>
+                <p>With patriotism high in pre-Civil-War New England, portraits of the first American President George Washington and the President Benjamin Harrison were common schoolhouse necessities. We plan to place these in Mr. Munn’s schoolhouse</p>
+            </section>
+            <section data-media="left">
+                <p><strong>McGuffey Readers</strong></p>
+                <figure>
+                    <img src="/resources/graphics/home/mcguffy-reader.jpg" alt="McGuffey Readers">
+                    <figcaption>McGuffey Readers <small>Oxford Historical Society Digital Photo Collection</small></figcaption>
+                </figure>
+                <p>McGuffey Readers were the standard classroom instruction books of American classrooms in 1850. Students advanced through books by reading level. The levels do not correspond to present-day reading student grades. The stories stressed American history, manners, animal, courage, honesty, God and love of reading. Children visitng Mr. Munn’s Schoolhouse will be able to read a variety of stories students would have enjoyed when Marcus S. Munn taught in this private boys school in Oxford.</p>
+            </section>
+            <section data-media="right">
+                <p><strong>DUNCE CAP AND STOOL</strong></p>
+                <figure>
+                    <img src="/resources/graphics/home/dunce-corner.jpg" alt="DUNCE CAP AND STOOL">
+                    <figcaption>DUNCE CAP AND STOOL <small>Oxford Historical Society Digital Photo Collection</small></figcaption>
+                </figure>
+                <p>A dunce cap and stool were considered essential supplies in an 1850’s one-room schoolhouse. The pointed hat was used as an article of discipline in schools in Europe and the United States especially in the 19th and early 20th centuries for children who were disruptive or were considered slow in learning. Students visiting the 1850’s Mr. Munn Schoolhouse will see this out-dated form of school discipline. We trust that the stool and cap will not be required in this century.</p>
+            </section>
+            <section>
+                <p><strong>30 Star American Flag</strong></p>
+                <figure>
+                    <img src="/resources/graphics/home/30-star-flag.jpg" alt="30 Star American Flag">
+                    <figcaption>30 Star American Flag <small>Oxford Historical Society Digital Photo Collection</small></figcaption>
+                </figure>
+                <p>In 1850, Mr. Munn’s Schoolhouse would have featured a 30-star flag. Accurate reproduction 30-star flags are available for $120. We hope our supporters will enable us to purchase one for the opening of the schoolhouse.</p>
+            </section>
+        </article>
+        <?php include($_SERVER['DOCUMENT_ROOT']."/resources/layout-modules/footer.php") ?>
+    </body>
+</html>
